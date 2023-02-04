ddef19241d2ecd3bcf0905f2d219ee307138ef12
tukaianirban <mukherjee.anirban86@gmail.com>
2023-02-04 22:41:46 +0100

    chore: removing unwanted errors

--- errs/defs.go
+++ errs/defs.go
@@ -3,15 +3,11 @@ package errs
 import "errors"
 
 var (
-	ErrBruceNotInit                      = errors.New("bruce not init")
-	ErrBruceTypeNotSupported             = errors.New("type not supported")
-	ErrGCPAccountError                   = errors.New("GCP account connection error")
-	ErrPSNContentNotFound                = errors.New("content not found")
-	ErrInvalidPSNContent                 = errors.New("PSN content is invalid")
-	ErrPushMessageContentStoreNotDefined = errors.New("push Message content store type not defined")
-	ErrPushMessageContentStoreNotInit    = errors.New("push message content store is not init")
-	ErrDeviceStoreNotInit                = errors.New("device store not init")
-	ErrDeviceStoreNotDefined             = errors.New("device store not defined")
-	ErrFCMClientNotInit                  = errors.New("fcm client is not init")
-	ErrFCMClientNotDefined               = errors.New("fcm client is not defined")
+	ErrBruceNotInit          = errors.New("bruce not init")
+	ErrBruceTypeNotSupported = errors.New("type not supported")
+	ErrGCPAccountError       = errors.New("GCP account connection error")
+	ErrDeviceStoreNotInit    = errors.New("device store not init")
+	ErrDeviceStoreNotDefined = errors.New("device store not defined")
+	ErrFCMClientNotInit      = errors.New("fcm client is not init")
+	ErrFCMClientNotDefined   = errors.New("fcm client is not defined")
 )
